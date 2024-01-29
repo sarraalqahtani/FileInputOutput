bbc25cb7d98da45ae19eadac169a3aae8a35cea2
alqahtas <alqahtas@wfu.edu>
2024-01-29 11:22:07 -0500

    Initial commit

---
+++ src/FileInputOutput.java
@@ -0,0 +1,14 @@
+public class FileInputOutput {
+
+    public static void main(String[] args) {
+//       1) Open the given file inside src called moviesDataSet.csv
+//       2) Read each row in the dataset
+//       3) Print each row to the screen
+//       4) Print only the name of the movie
+//       5) Add exception handling (try & catch) when needed
+//       6) Close the files after finishing the processing
+//       7) Commit your code and push it to the GitHub
+
+    }
+
+}
